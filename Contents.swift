c5a74a1ecc5517592623cb92ce5339d9bd32c5a6
Binod Shrestha <shresthabinod.ca@gmail.com>
2020-09-09 17:59:19 -0400

    type safety examples added

---
+++ Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+// type inference
+var age = 40
+
+// type annotation
+var my_name: String
+my_name = "Binod Shrestha"
+
+// type annotation + inference
+var full_name: String = "Binod Shrestha"
+// multiple variables - type annotation shorthand
+var my_age, phone, dob, street_no: Int
+my_age = 40
+phone = 6477734536
+dob = 2012
+street_no = 6
