093406a5c7b362672590c9928453d060b8d0516e
Binod Shrestha <shresthabinod.ca@gmail.com>
2020-09-10 13:41:34 -0400

    conflicts solved

--- Contents.swift
+++ Contents.swift
@@ -1,4 +1,60 @@
 import UIKit
+// Swift Arrays
+//creating arrays
+var kolor: [String] = ["Red", "Green", "White", "Black"]
+var kolor2: Array<String> = Array<String>()
+var mostPrefered1 = kolor[0]
+kolor[0] = "Green"
+var mostPrefered2 = kolor[0]
+// manupulation of array
+
+// adding the last element
+kolor.append("Yellow")
+kolor += ["Brown","Pink"]
+
+//inserting using index
+kolor.insert("Light Green", at: 2)
+kolor.insert("Red", at: 0)
+kolor.remove(at: 1)
+kolor.reverse()
+kolor.sort()
+var reveresedClasses = kolor.reversed()
+var kolorSored = kolor.sorted()
+kolor.contains("Black")
+
+//2D Arrays
+var TwoDArrays:[[String]] = [
+    ["Apple", "Mango,","Orange"],
+    ["Soda","Juice", "Beer"]
+]
+
+var item = TwoDArrays[0][1]
+var item2 = TwoDArrays[1][0]
+
+
+//=======================================
+//==========Dictionaries=================
+//=======================================
+// creating dictionaries
+var bookStore:[String:Int] = [:]
+var bookStore2:[String:Int] = ["Davis":1, "Trafalgar":2, "HMC":3]
+
+// All keys and Values
+var storeKeys = [String](bookStore2.keys)
+var storeValues = [Int](bookStore2.values)
+
+//=========SET============
+var newSet: Set = ["apple","orange","mango","banana"]
+var newSet2: Set = ["apple","pineapple","Kiwi","mango"]
+var commonItem = newSet.intersection(newSet2)
+var subTracting = newSet.subtracting(newSet2)// remaining from newSet
+var notCommonItem = newSet.symmetricDifference(newSet2)
+var allItems = newSet.union(newSet2)
+newSet.remove("apple")
+newSet.insert("avocado")
+newSet.contains("orange")
+newSet.sorted()
+print(newSet)
 
 // type inference
 var age = 40
@@ -52,5 +108,4 @@ var currentMana:Double = 3.20
 var manaPercentage = String(currentMana/100)
 var debugStats = "\(characterName)\n\nPrefers: \(weaponClassification)\nCurrent Mana: \(manaPercentage)"
 
->>>>>>> operators
 
